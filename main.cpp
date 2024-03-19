eb8056511d4a75ceac39eb96286c18a24f08dcc7
nahidsoft <hnhasan0027@gmail.com>
2024-03-20 02:08:10 +0600

    initial program

---
+++ main.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+double a,b;
+char choice;
+cout<<"Enter your first and  digit :";
+cin>>a;
+cout<<"Enter your 2nd digit :";
+cin>>b;
+cout<<"Enter your operation(+,-,*,/) :";
+cin>>choice;
+if(choice=='+'){
+    double sum=a+b;
+    cout<<"result is :"<<sum<<endl;
+}
+else if(choice=='-'){
+
+    double sub=a-b;
+    cout<<"result is :"<<sub<<endl;
+}
+else  if(choice=='*'){
+
+    double mul=a*b;
+    cout<<"result is :"<<mul<<endl;
+}
+
+else if(choice=='/'){
+    if(b != 0){
+    double div=(double)a/b;
+    cout<<"result is :"<<div<<endl;
+    }
+    else{
+        cout<<"can not divite by zero"<<endl;
+    }
+}
+else{
+    cout<<"not the number ";
+}
+
+
+
+
+return 0;
+}
